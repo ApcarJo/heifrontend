b52243cca94dffef55849732d4de9e6194fd6794
José Luis <hansokaz@gmail.com>
2021-09-07 13:16:25 +0200

    fixed Home behavior

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,10 +1,29 @@
-import GWupdate from '../GWupdate/GWupdate';
 
-const Home = () => 
-    (
+import GWupdate from '../GWupdate/GWupdate';
+import { useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const Home = (props) => {
+
+    let history = useHistory();
+    
+if (props.credentials.user?.name) {
+    return (
         <div className="viewHome">
             <GWupdate></GWupdate>
         </div>
-    ) 
+    )
+} else {
 
-export default Home;
+    history.push("login");
+    return (
+        "Loading Login View"
+    )
+    
+}
+
+} 
+
+export default connect((state) => ({
+    credentials: state.credentials
+}))(Home);
